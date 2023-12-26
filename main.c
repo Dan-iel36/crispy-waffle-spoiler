c33ad88c7198dda4a431d55d75b2a1d627dc12da
elite <elite@MacBook-Air-de-Admin0001.local>
2023-12-26 14:47:06 +0100

    last

--- main.c
+++ main.c
@@ -9,55 +9,52 @@
 #include <pthread.h>
 #include "job.h"
 
+// Define constants for the server port, maximum clients, printer port, and printer host
 #define PORT 8080
 #define MAX_CLIENTS 5
 #define PRINTERPORT 8088
 #define PRINTERHOST "127.0.0.1"
 
+// Function to forward a job to a printer server
 void* jobForwarder(struct job* myjob){
     int sock = 0;
     struct sockaddr_in serv_addr;
 
-    // Création du socket client
+    // Create a socket for the client
     if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
         perror("Échec de la création du socket client");
         exit(EXIT_FAILURE);
     }
 
-    //Sockets et TCP/IP
+    // Set up the socket and TCP/IP
     serv_addr.sin_family = AF_INET;
     serv_addr.sin_port = htons(PRINTERPORT);
 
-    // Convertir l'adresse IPv4 et affecter au socket client
+    // Convert the IPv4 address and assign it to the client socket
     if (inet_pton(AF_INET, PRINTERHOST,
                   &serv_addr.sin_addr) <= 0) {
         perror("Adresse invalide / Adresse non supportée");
         exit(EXIT_FAILURE);
     }
 
-    // Connexion au serveur
+    // Connect to the server
     if (connect(sock, (struct sockaddr *) &serv_addr,
                 sizeof(serv_addr)) < 0) {
         perror("Connexion échouée");
         exit(EXIT_FAILURE);
     }
 
+    // Send the job to the printer server
+    send(sock, myjob, sizeof(struct job), 0);
+    printf("%d, %d, %d, %s here for send\n", sock, myjob->priority, myjob->pages, myjob->printerId);
+    fflush(stdout);
+    sleep(rand() % 20 + 1);
 
-
-    //while(1){
-        send(sock, myjob, sizeof(struct job), 0);
-        printf("%d, %d, %d, %s here for send\n", sock, myjob->priority, myjob->pages, myjob->printerId);
-        fflush(stdout);
-        sleep(rand() % 20 + 1);
-
-        //close(sock);
-        //breakout of loop
-
-    //}
-close(sock);
+    close(sock);
     return NULL;
 }
 
+// Function to process a job in a separate thread
 void* treatJobWithAThread(void* arg){
     struct job* buffer = (struct job*) arg;
     FILE *printerFile;
@@ -83,11 +80,8 @@ void* treatJobWithAThread(void* arg){
     jobForwarder(buffer);
 
     free(buffer);
-    //close(sock);
     return NULL;
 }
-
-
 
 int main() {
     int server_fd, new_socket, valread;
@@ -95,16 +89,19 @@ int main() {
     int opt = 1;
     socklen_t addrlen = sizeof(address);
 
+    // Create a socket for the server
     if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
         perror("Échec de la création du socket serveur");
         exit(EXIT_FAILURE);
     }
 
+    // Set socket options
     if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt))) {
         perror("setsockopt");
         exit(EXIT_FAILURE);
     }
 
+    // Bind the socket to a specific IP address and port
     address.sin_family = AF_INET;
     address.sin_addr.s_addr = INADDR_ANY;
     address.sin_port = htons(PORT);
@@ -114,20 +111,24 @@ int main() {
         exit(EXIT_FAILURE);
     }
 
+    // Start listening for incoming connections
     if (listen(server_fd, MAX_CLIENTS) < 0) {
         perror("Échec de l'écoute du socket serveur");
         exit(EXIT_FAILURE);
     }
 
+    // Loop to accept incoming connections
     while (1) {
         if ((new_socket = accept(server_fd, (struct sockaddr *)&address, &addrlen)) < 0) {
             perror("Échec de l'acceptation de la connexion");
             exit(EXIT_FAILURE);
         }
 
+        // Allocate memory for the job buffer
         struct job* buffer = (struct job*) malloc(sizeof(struct job));
         memset(buffer, 0, sizeof(struct job));
 
+        // Loop to receive data from the client
         while(1) {
             int bytes_received = recv(new_socket, buffer, sizeof(struct job), 0);
             if (bytes_received < 0) {
@@ -139,6 +140,7 @@ int main() {
                 exit(EXIT_FAILURE);
             }
 
+            // If received data size is zero, it means the client has closed the connection
             if(bytes_received == 0) {
                 printf("connection closed by client");
                 break; // Connection closed by client
@@ -148,11 +150,10 @@ int main() {
             struct job* buffer_copy = (struct job*) malloc(sizeof(struct job));
             memcpy(buffer_copy, buffer, sizeof(struct job));
 
+            // Create a new thread to process the job
             pthread_create(&thread_id, NULL, treatJobWithAThread, buffer_copy);
-            //pthread_join(thread_id, NULL);
 
             printf("Received: Printer ID: %s, Pages: %d, Priority: %d, ThreadInCharge: %lu \n", buffer->printerId, buffer->pages, buffer->priority,  (unsigned long int) thread_id);
-
         }
 
         free(buffer);
